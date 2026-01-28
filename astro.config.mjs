754c2fe8ef68be8ac235c40da39fe7200fab4d9d
xiaoshuu <jiangruoxin@163.com>
2026-01-28 22:48:09 +0800

    change web

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    // 允许 HTML 
+    extendDefaultPlugins: true,     
+    // 添加 remark 插件
+    remarkPlugins: [remarkMath],   
+    // 添加 rehype 插件
+    rehypePlugins: [rehypeKatex],
+  },
+});
